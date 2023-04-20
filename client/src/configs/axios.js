48b1857000cf6dd554ec477fbd49b1dcc9b74f21
MrYasuo <buihai0706vn@gmail.com>
2023-04-20 08:54:06 +0700

    :bug: Fix axios credential

--- client/src/configs/axios.js
+++ client/src/configs/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const instance = axios.create({
 	baseURL:
-		process.env.REACT_NODE_ENV === "production"
+		process.env.REACT_APP_NODE_ENV === "production"
 			? "https://youtube-fullstack-384218.de.r.appspot.com/api"
 			: "http://localhost:5000/api",
 	withCredentials: true,
